c18ad6179eb3f7fb3691ccdae0815634f4393d38
viralwebelight <69192741+viralwebelight@users.noreply.github.com>
2021-09-30 11:57:40 +0530

    Revert "20thsept"

--- Makemyparadise/src/App.js
+++ Makemyparadise/src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import Login from './components/login';
 import Dashboard from "./components/dashboard"
-import Saas from "./components/saas"
-import Birthday from './components/birthdayreminder';
 
 
 function App() {
   return (
    <>
    {/* <Login/>  */}
-   {/* <Dashboard/> */}
-   {/* <Saas/> */}
-   <Birthday/>
+   <Dashboard/>
    </>
   );
 }
